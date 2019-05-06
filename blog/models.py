726aa867d6386c9fb3a6f1cdf1ddd279f85804d5
^=g#RPo#YWE@7:b3YAJ7,.=3E,tN8*quQtyN3^y-B4FUxt]2x4f9Xtt%kn <evgen.dyudyunov@gmail.com>
2019-05-06 19:57:25 +0300

    + blog app

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def summery(self):
+        return self.body[:140] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,5 +22,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
